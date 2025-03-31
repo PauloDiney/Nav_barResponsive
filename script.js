621fca9654a9dc48d86620aeccbe659975e342c0
PauloDiney <paulo19martin@gmail.com>
2025-03-30 23:03:10 -0300

    Nav_bar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".menu-button").addEventListener("click", function() {
+    document.querySelector(".nav_container").classList.toggle("active");
+
+    document.querySelector(".menu-button").classList.toggle("toggle");
+
+});
